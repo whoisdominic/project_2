b0528668ec5788e2a786f3fe5c37b78289894ef0
Dominic Cobb <dominicshajaa@icloud.com>
2020-05-27 12:25:39 -0700

    search styled more

--- views/Search.jsx
+++ views/Search.jsx
@@ -11,23 +11,52 @@ class Index extends React.Component {
         <NavComp></NavComp>
 
         <div className="main-cont">
-          <form action="/search" method="POST">
-            <input type="search" name="search" />
-            <input type="submit" value="Search" />
-          </form>
+          <div className="search-cont">
+            <form className="search" action="/search" method="POST">
+              <input className="grey lighten-3" type="search" name="search" />
+              <input
+                className="btn green accent-4"
+                type="submit"
+                value="Search"
+              />
+            </form>
+            <div className="spotify-cont">
+              <h4>POWERED BY</h4>
+              <img
+                className="spotify-logo"
+                src="/img/spotify-logo-png-7072.png"
+                alt="spotify logo"
+              />
+            </div>
+          </div>
+          <div className="about">
+            <div className="about-1">
+              <h2>How it works</h2>
+              <a href="https://www.dictionary.com/e/slang/g-o-a-t/">
+                What is a GOAT anyway?
+              </a>
+              <p>
+                <br /> Here you can search for your favorite artist just like
+                you would on spotify! <br />
+                <br />
+                Once you find your favorite artist. Take a look at the
+                categories and see if you think they are worthy of a greatest of
+                all time vote in that genre.
+                <br />
+              </p>
+            </div>
+            <div className="about-2">
+              <p>
+                Most votes in a category will make that artist the GOAT of that
+                genre! <br /> <br /> Goats are displayed in their respective
+                category pages!
+              </p>
+            </div>
+          </div>
         </div>
       </Template>
     );
   }
 }
-
-/////////////////////////////////////////////////
-/*
-Adds the current top 100 || 10 to the dom    
-
-
-
-
-*/
 
 module.exports = Index;
